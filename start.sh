7151bbb7e1c1fc823fa5f6408087a933001641a5
Henric Andersson <git@sensenet.nu>
2019-12-28 00:41:14 +0000

    Use smarter detection

--- start.sh
+++ start.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+UUID='ftE13S1G'
+FILTER='ttyUSB*'
+
 while true; do
-	# Locate the correct ttyACM for IR Deluxe
-	TTY=$(dmesg | grep "FTDI" | grep attached | tail -n 1 | egrep -oe 'ttyUSB[0-9]')
-	if [ $? -eq 0 -a -e /dev/${TTY} ]; then
-		./server.py --tty /dev/${TTY}
-		echo "Server terminated"
-	fi
-	echo "No USB-2-Serial device detected!"
+	# Locate the correct ttyUSB for the projector
+	for DEV in $(find /dev -name ${FILTER}); do
+		if udevadm info -a --name $DEV | grep -q "$UUID"; then
+			echo "Found it: $DEV"
+			./server.py --tty $DEV
+			echo "Server terminated"
+		fi
+	done
+
 	echo "Sleeping 1s and trying again"
 	sleep 1s
 done
